5112e560629dded981a103fbcbb0e4ceb3596f4a
Divyesh Patel <dev.eth007@gmail.com>
2024-01-24 14:53:06 +0530

    change origin in socket cors

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT
 const app = express();
 
 app.use(cors({
-    origin: ["*",'https://chat-app-swart-pi.vercel.app',"http://localhost:5173"],
+    origin: ["*",'https://chat-app-swart-pi.vercel.app/',"http://localhost:5173"],
     credentials: true  // If your requests include credentials (cookies, HTTP authentication), set this to true
 }));
 app.use(express.json());
@@ -21,12 +21,13 @@ connectDB().then(() => {
         console.log(`Server is running at port: ${PORT} http://localhost:${PORT}`)
     })
     const io = new Server(server, {
-        cors: ["*","https://chat-app-swart-pi.vercel.app","http://localhost:5416"],
+        cors: ["https://chat-app-swart-pi.vercel.app/","http://localhost:5173"],
         credentials: true,
     });
 
     global.onlineUsers = new Map();
     io.on("connection", (socket) => {
+        console.log("CONNECTED")
         global.chatSocket = socket;
         socket.on("add-user", (userId) => {
             onlineUsers.set(userId, socket.id);
